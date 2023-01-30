ef5ae8985f3a5cec96a635592ab878566677b243
Loc-ed <41115064+loc-ed@users.noreply.github.com>
2023-01-30 20:12:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import initiateScrape from './shoeScrape';
-import initiateHardwareScrape from './hardwareScrape';
+const initiateScrape = require('./shoeScrape')
+const initiateHardwareScrape = require('./hardwareScrape')
 
 const PORT = 8000
-const button = document.querySelector("#myButton");
-import express from 'express';
-import chalk from 'chalk';
+// const button = document.querySelector("#myButton");
+const express = require('express')
+const chalk = require('chalk')
 
-function initiateSequence() {
-    const app = express()
-    var server = app.listen(PORT, async function () {
-        console.log(`server running on port ${PORT} `)
-        initiateScrape()
-        initiateHardwareScrape()
-    })
-}
-// const app = express()
-// var server = app.listen(PORT, async function () {
-//     console.log(`server running on port ${PORT} `)
-//     initiateScrape()
-//     initiateHardwareScrape()
-// })
+// function initiateSequence() {
+//     const app = express()
+//     var server = app.listen(PORT, async function () {
+//         console.log(`server running on port ${PORT} `)
+//         initiateScrape()
+//         initiateHardwareScrape()
+//     })
+// }
+const app = express()
+var server = app.listen(PORT, async function () {
+    console.log(`server running on port ${PORT} `)
+    initiateScrape()
+    initiateHardwareScrape()
+})
 
-button.addEventListener("click", initiateSequence())
+// button.addEventListener("click", initiateSequence())
 
 
 
