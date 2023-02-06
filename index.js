1e26e7ffe7c880c907fd8975da13177dbf55f449
Loc-ed <41115064+loc-ed@users.noreply.github.com>
2023-02-06 15:35:41 +0200

    debugging index.js

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ import initiateScrape from './shoeScrape'
 import initiateHardwareScrape from './hardwareScrape'
 
 const PORT = 8000
-import express from 'express'
-import chalk from 'chalk'
+// import express from 'express'
+// import chalk from 'chalk'
 
 const button = document.querySelector('.glowing-btn');
 
 function initiateSequence() {
 
+    console.log('debug mode')
     // initiateScrape()
     // initiateHardwareScrape()
-    const app = express()
-    var server = app.listen(PORT, async function () {
-        console.log(`server running on port ${PORT} `)
-        initiateScrape()
-        initiateHardwareScrape()
-    })
+    // const app = express()
+    // var server = app.listen(PORT, async function () {
+    //     console.log(`server running on port ${PORT} `)
+    //     initiateScrape()
+    //     initiateHardwareScrape()
+    // })
 }
 
 button.addEventListener('click', function() {
