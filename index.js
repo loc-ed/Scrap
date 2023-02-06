1039e9a01a06e92fee37b70422eb60f56e13251b
Loc-ed <41115064+loc-ed@users.noreply.github.com>
2023-02-06 14:18:26 +0200

    fresh copy

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const initiateScrape = require('./shoeScrape')
-const initiateHardwareScrape = require('./hardwareScrape')
+import initiateScrape from './shoeScrape'
+import initiateHardwareScrape from './hardwareScrape'
 
 const PORT = 8000
-// const button = document.querySelector("#myButton");
-const express = require('express')
-const chalk = require('chalk')
+import express from 'express'
+import chalk from 'chalk'
+
+const button = document.querySelector('.glowing-btn');
 
 function initiateSequence() {
+
+    // initiateScrape()
+    // initiateHardwareScrape()
     const app = express()
     var server = app.listen(PORT, async function () {
         console.log(`server running on port ${PORT} `)
@@ -14,6 +18,10 @@ function initiateSequence() {
         initiateHardwareScrape()
     })
 }
+
+button.addEventListener('click', initiateScrape)
+
+
 // const app = express()
 // var server = app.listen(PORT, async function () {
 //     console.log(`server running on port ${PORT} `)
