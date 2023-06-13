1f61d7edcb34662a9d5a361b5d236ed7d716f7dd
Loc-ed <41115064+loc-ed@users.noreply.github.com>
2023-06-13 11:06:33 +0200

    serverless functions returning appropraite response with netlify timeout

--- README.md
+++ README.md
@@ -1,18 +1,5 @@
 ## Just another climbing scrape 
-Web scraping tool that grabs product information of climbing shoe brands
 
-Featuring : Cheerio + Axios + Pagination Magic + Elbow Grease
+Web scraping tool that grabs product information of climbing gear locally and internationally 
 
-Featured Brands = [MadRock, Evolv, Salewa, La Sportiva, Scarpa]
-
-## Build Setup
-
-1. install dependencies
-
-~ npm install
-
-2. serve app in console
-
-~ npm run start
-
-Scrape compiles and generates the data into a CSV format 
+Featuring : Cheerio + Axios + Netlify Serverless Functions
--- index.js
+++ index.js
@@ -1,51 +1,145 @@
 // import initiateScrape from './shoeScrape.js'
 // import initiateHardwareScrape from './hardwareScrape.js'
 
-const PORT = 8000
+// const { response } = require("express")
+
+// const PORT = 8000
 // import express from 'express'
 // const express = require('express')
 // import chalk from 'chalk'
 
+// const chalk = require('chalk')
+// const parseDocument = require('htmlparser2')
+// const write = require('fast-csv')
+// const createWriteStream = require('fs')
+
+// import  axios  from 'axios';
+// import { response } from 'express';
+// import { load } from 'cheerio';
+// import chalk from 'chalk';
+// import { parseDocument } from 'htmlparser2';
+// import { write } from 'fast-csv';
+// import { createWriteStream } from "fs";
+
+// let currentDate = new Date().toJSON().slice(0, 10);
+// const start = Date.now()
+// const filename = `Hardware(${currentDate}).csv`
+// const writableFile = createWriteStream(filename);
+
+const HEADER = [{
+    name : 'Product',
+    currency : 'Currency',
+    price : 'Price',
+    brand : 'Brand',
+    catagory : 'Catagory',
+    sex : 'Gender',
+    location : 'Location',
+    supplier : 'Supplier',
+}]
+
+
 const button = document.querySelector('.glowing-btn');
 
-function initiateSequence() {
+    button.addEventListener('click', async () => {
 
-    console.log('initiate sequence mode')
-    alert('The button has been clicked!');
-    // initiateScrape()
-    // initiateHardwareScrape()
-    // const app = express()
-    // var server = app.listen(PORT, async function () {
-    //     console.log(`server running on port ${PORT} `)
-    //     initiateScrape()
-    //     initiateHardwareScrape()
-    // })
-}
+        var hardwareFormatted = []
+        var shoesFormatted = []
+        const shoeResponse = await fetch('/.netlify/functions/shoeScrapeLocal')
+                        .then(response => response.json()
+        )
+        const hardwareResponse = await fetch('/.netlify/functions/hardwareScrapeLocal')
+                        .then(response => response.json()
+        )
 
-function alertMe(){
-      console.log('clicked ')
-      alert('The button has been clicked!');
-    }
-    
-    button.addEventListener('click', initiateSequence)
-    // button.addEventListener('click', alertMe);
+        const shoeArray = shoeResponse.content
+        const hardwareArray = hardwareResponse.content
 
-// button.addEventListener('click', function() {
-//     console.log('test')
-//     initiateSequence()
-// })
+        console.log('testing the button with single scrape ')
+        // responseText.innerText = JSON.stringify(response)
+        // console.log(shoeResponse)
+        // console.log(hardwareResponse)
+        // console.log(hardwareArray[0])
+
+        hardwareArray.forEach((item) => {
+            hardwareFormatted.push({
+                name : item.name,
+                currency : item.currency,
+                price : item.price,
+                brand : item.brand,
+                catagory : item.catagory,
+                sex : item.sex,
+                location : item.location,
+                supplier : item.supplier,
+            })
+        })
+
+        shoeArray.forEach((item) => {
+            shoesFormatted.push({
+                name : item.name,
+                currency : item.currency,
+                price : item.price,
+                brand : item.brand,
+                catagory : item.catagory,
+                sex : item.sex,
+                location : item.location,
+                supplier : item.supplier,
+            })
+        })
+    })
 
 
-// const app = express()
-// var server = app.listen(PORT, async function () {
-//     console.log(`server running on port ${PORT} `)
-//     initiateScrape()
-//     initiateHardwareScrape()
-// })
+//------------------------------------------------------------------------------------------------//
 
-// button.addEventListener("click", initiateSequence())
+// COMPILE DATA
+
+// async function compileShoeData() {
+
+//     shoeList = HEADER.concat(sportivaData,driftersData,mmoData,togData,ramData,adventureInc, evolData[0],evolData[1], scarpaData,madrockData, salewaData[0], salewaData[1])
+//     // shoeList = HEADER.concat(evolData[0],evolData[1])
+//     // console.log(shoeList)
+//     // shoeCSV = convertToCSV(shoeList)
+//     // write(shoeList).pipe(writableFile)
+//     // console.log(chalk.green.bold(` Shoe CSV has been successfully generated`))
+//     // const end = Date.now()
+//     // let time = (end - start)/1000
+//     // console.log(chalk.blue.bold(`Execution time: ${time} seconds`))
+
+//     return
+// }
+//------------------------------------------------------------------------------------------------//
+
+//GENERATE CSV 
+ 
+// function convertToCSV(shoeData) {
+
+//     const objArray = JSON.stringify(shoeData);
+//     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+//     var str = '';
+
+//     for (var i = 0; i < array.length; i++) {
+//         var line = '';
+//         for (var index in array[i]) {
+//             if (line != '') line += ','
+
+//             line += array[i][index];
+//         }
+
+//         str += line + '\r\n';
+//     }
+//     return str;
+// }
 
 
+ // COMPILE DATA
 
+// function compileData() {
 
+//     const hardwareList = HEADER.concat(mmoData,driftersData,ramData)
+//     write(hardwareList).pipe(writableFile)
+//     console.log(green.bold(`Hardware CSV has been successfully generated`))
+//     const end = Date.now()
+//     let time = (end - start)/1000
+//     console.log(blue.bold(`Execution time: ${time} seconds`))
+
+// }
 
