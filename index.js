5c8ecabc8cd670d0f8e92a0c166d8036abb9ca11
Loc-ed <41115064+loc-ed@users.noreply.github.com>
2023-06-13 11:55:34 +0200

    added downloaded functionality for scraped data

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-// import initiateScrape from './shoeScrape.js'
-// import initiateHardwareScrape from './hardwareScrape.js'
+let currentDate = new Date().toJSON().slice(0, 10);
+const shoeFilename = `ShoeData(${currentDate}).csv`
+const hardwareFilename = `HardwareData(${currentDate}).csv`
 
-// const { response } = require("express")
-
-// const PORT = 8000
-// import express from 'express'
-// const express = require('express')
-// import chalk from 'chalk'
-
-// const chalk = require('chalk')
-// const parseDocument = require('htmlparser2')
-// const write = require('fast-csv')
-// const createWriteStream = require('fs')
-
-// import  axios  from 'axios';
-// import { response } from 'express';
-// import { load } from 'cheerio';
-// import chalk from 'chalk';
-// import { parseDocument } from 'htmlparser2';
-// import { write } from 'fast-csv';
-// import { createWriteStream } from "fs";
-
-// let currentDate = new Date().toJSON().slice(0, 10);
-// const start = Date.now()
-// const filename = `Hardware(${currentDate}).csv`
-// const writableFile = createWriteStream(filename);
-
-const HEADER = [{
+const HEADER = {
     name : 'Product',
     currency : 'Currency',
     price : 'Price',
@@ -35,8 +11,53 @@ const HEADER = [{
     sex : 'Gender',
     location : 'Location',
     supplier : 'Supplier',
-}]
+}
 
+function convertToCSV(objArray) {
+
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+    return str;
+}
+
+function exportCSVFile(headers, items, fileTitle) {
+    
+    if (headers) {
+        items.unshift(headers)
+    }
+
+    var jsonObject = JSON.stringify(items)
+    var csv = convertToCSV(jsonObject)
+
+    var blob = new Blob([csv], {type: 'text/csv;charset=utf-8;'})
+    if (window.navigator && window.navigator.msSaveBlob) { // for IE
+        window.navigator.msSaveBlob(blob, fileTitle)
+    }
+    else {
+        var link = document.createElement("a")
+        if (link.download !== undefined) {
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob)
+            link.setAttribute("href", url)
+            link.setAttribute("download", fileTitle)
+            link.style.visibility = 'hidden'
+            document.body.appendChild(link)
+            link.click()
+            document.body.removeChild(link)
+        }
+    }
+}
 
 const button = document.querySelector('.glowing-btn');
 
@@ -53,13 +74,7 @@ const button = document.querySelector('.glowing-btn');
 
         const shoeArray = shoeResponse.content
         const hardwareArray = hardwareResponse.content
-
-        console.log('testing the button with single scrape ')
-        // responseText.innerText = JSON.stringify(response)
-        // console.log(shoeResponse)
-        // console.log(hardwareResponse)
-        // console.log(hardwareArray[0])
-
+        
         hardwareArray.forEach((item) => {
             hardwareFormatted.push({
                 name : item.name,
@@ -85,61 +100,8 @@ const button = document.querySelector('.glowing-btn');
                 supplier : item.supplier,
             })
         })
+
+        exportCSVFile(HEADER,shoesFormatted,shoeFilename)
+        exportCSVFile(HEADER,hardwareFormatted,hardwareFilename)
     })
-
-
-//------------------------------------------------------------------------------------------------//
-
-// COMPILE DATA
-
-// async function compileShoeData() {
-
-//     shoeList = HEADER.concat(sportivaData,driftersData,mmoData,togData,ramData,adventureInc, evolData[0],evolData[1], scarpaData,madrockData, salewaData[0], salewaData[1])
-//     // shoeList = HEADER.concat(evolData[0],evolData[1])
-//     // console.log(shoeList)
-//     // shoeCSV = convertToCSV(shoeList)
-//     // write(shoeList).pipe(writableFile)
-//     // console.log(chalk.green.bold(` Shoe CSV has been successfully generated`))
-//     // const end = Date.now()
-//     // let time = (end - start)/1000
-//     // console.log(chalk.blue.bold(`Execution time: ${time} seconds`))
-
-//     return
-// }
-//------------------------------------------------------------------------------------------------//
-
-//GENERATE CSV 
- 
-// function convertToCSV(shoeData) {
-
-//     const objArray = JSON.stringify(shoeData);
-//     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-//     var str = '';
-
-//     for (var i = 0; i < array.length; i++) {
-//         var line = '';
-//         for (var index in array[i]) {
-//             if (line != '') line += ','
-
-//             line += array[i][index];
-//         }
-
-//         str += line + '\r\n';
-//     }
-//     return str;
-// }
-
-
- // COMPILE DATA
-
-// function compileData() {
-
-//     const hardwareList = HEADER.concat(mmoData,driftersData,ramData)
-//     write(hardwareList).pipe(writableFile)
-//     console.log(green.bold(`Hardware CSV has been successfully generated`))
-//     const end = Date.now()
-//     let time = (end - start)/1000
-//     console.log(blue.bold(`Execution time: ${time} seconds`))
-
-// }
 
